d869cd2ab4fbbc6db7159cea99aa10c05976e532
chris <christopher.ridder@gmail.com>
2023-12-17 10:54:29 +0100

    Converter Arabic to Roman Numbers

---
+++ RomanNumbers/src/main/java/org/example/Converter.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Converter {
+    private static final String HUNDREDS[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    private static final String TENS[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    private static final String ONES[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+
+    public String convert(int number, String result) {
+
+        if (number > 999) return "-1";
+
+        result = result + HUNDREDS[number / 100];
+        number = number % 100;
+
+        result = result + TENS[number / 10];
+        number = number % 10;
+
+        result = result + ONES[number];
+
+        return result;
+    }
+
+}
---
+++ RomanNumbers/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Converter().convert(1122, ""));  //0 < number < 1000
+    }
+}
